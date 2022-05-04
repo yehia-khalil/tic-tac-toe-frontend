bada8ddbc993f6d5fab4e5f67f847776b3444157
yehia-khalil <yehiakhalilykk@gmail.com>
2022-05-04 13:36:32 +0200

    Added menu styles in css file

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./menu.css"
 
 export default function Menu() {
   return (
     <div>
         Menu
-        <nav    
-            style={{
-                borderBottom: "solid 1px",
-                paddingBottom: "1rem",
-            }}>
+        <nav>
                 <Link to="/new">New Game</Link> | {" "}
                 <Link to="/scores">Scores</Link>
         </nav>
